353d507535ee80e7e55d8e01c14dc16d66ee0a02
Louisville-KY <louik.16161@gmail.com>
2025-10-28 01:02:54 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "", //Ice~2xBhLc
+  OWNER_NUMBER: "263719647303", // 256781793989
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
 
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
+// CREATED BY  Louis 
