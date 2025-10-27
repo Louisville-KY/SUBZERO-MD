34529fefa404ae6ef8dd4fe30504bb2d20f40995
Louisville-KY <louik.16161@gmail.com>
2025-10-28 01:09:06 +0300

    Update settings.js for Louis

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", //Ice~2xBhLc
-  OWNER_NUMBER: "263719647303", // 256781793989
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "Ice~2xBhLc",
+  OWNER_NUMBER: "+256781793989", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY  Louis 
